bb12e64ab7205aee7dd7e625060fce026664f057
nikollllllas <nikollasohta2014@gmail.com>
2024-01-12 15:58:02 -0300

    :sparkles:: add role column to user table on migration

--- api/src/database/knex/migrations/20230811164833_createUsers.js
+++ api/src/database/knex/migrations/20230811164833_createUsers.js
@@ -5,6 +5,14 @@ exports.up = (knex) =>
     table.text('email').notNullable()
     table.text('password').notNullable()
 
+    table
+      .enum('role', ['admin', 'customer'], {
+        useNative: true,
+        enumName: 'roles'
+      })
+      .notNullable()
+      .defaultTo('customer')
+
     table.timestamp('created_at').default(knex.fn.now())
     table.timestamp('updated_at').default(knex.fn.now())
   })
