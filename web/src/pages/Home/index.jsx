81c4b6eb9b0b6c947a0db4908af63977f2c795a0
nikollllllas <nikollasohta2014@gmail.com>
2024-01-15 14:14:01 -0300

    :sparkles:: updated home page to dynamically show components

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,10 +1,15 @@
 import { FiTruck, FiTag, FiShoppingCart } from 'react-icons/fi'
 
+import { useAuth } from '../../hooks/auth'
+import { USER_ROLE } from '../../utils/rules'
+
 import { Container } from './styles'
-import { Feature } from '../../components/Feature'
 import { Header } from '../../components/Header'
+import { Feature } from '../../components/Feature'
 
 export function Home() {
+  const { user } = useAuth()
+
   return (
     <Container>
       <Header />
@@ -15,16 +20,21 @@ export function Home() {
           icon={FiTag}
           to='/product'
         />
-        <Feature
-          title='Fornecedores'
-          icon={FiTruck}
-          to='/suppliers'
-        />
-        <Feature
-          title='Relatório de vendas'
-          icon={FiShoppingCart}
-          to='/sales-report'
-        />
+
+        {[USER_ROLE.ADMIN] && (
+          <Feature
+            title='Fornecedores'
+            icon={FiTruck}
+            to='/suppliers'
+          />
+        )}
+        {[USER_ROLE.ADMIN, USER_ROLE.SELLER].includes(user.role) && (
+          <Feature
+            title='Relatório de vendas'
+            icon={FiShoppingCart}
+            to='/sales-report'
+          />
+        )}
       </main>
     </Container>
   )
