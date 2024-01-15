eabd5736c47dd9f0ae170270265d2bc4298fd34a
nikollllllas <nikollasohta2014@gmail.com>
2024-01-15 15:47:49 -0300

    :recycle:: updated auth in signin to hidden password and use cookies

--- api/src/server.js
+++ api/src/server.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ['http://localhost:3333', 'http://127.0.0.1:3333'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     credentials: true
   })
 )
