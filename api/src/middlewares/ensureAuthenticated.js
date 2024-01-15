3e8d03c31ce5810c088c44a6bcc4dd58f96871f6
nikollllllas <nikollasohta2014@gmail.com>
2024-01-15 15:54:31 -0300

    :recycle:: using cookies in auth

--- api/src/middlewares/ensureAuthenticated.js
+++ api/src/middlewares/ensureAuthenticated.js
@@ -9,7 +9,7 @@ function ensureAuthenticated(request, response, next) {
     throw new AppError('JWT token não informado', 401)
   }
 
-  const [, token] = authHeader.split('token=')
+  const [, token] = authHeader.cookie.split('token=')
 
   try {
     const { sub: user_id, role } = verify(token, authConfig.jwt.secret)
