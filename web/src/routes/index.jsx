29492834e648c8bcb77db21a4477612c079bc84c
nikollllllas <nikollasohta2014@gmail.com>
2024-01-15 13:43:05 -0300

    :recycle:: updating routes

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -2,11 +2,11 @@ import { BrowserRouter } from 'react-router-dom'
 
 import { useAuth } from '../hooks/auth'
 
-import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
+import { AdminRoutes } from './admin.routes'
 
 export function Routes() {
   const { user } = useAuth()
 
-  return <BrowserRouter>{user ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>
+  return <BrowserRouter>{user ? <AdminRoutes /> : <AuthRoutes />}</BrowserRouter>
 }
